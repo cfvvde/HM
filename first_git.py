985d37bc30fe806e121b392319500720ad2f29e4
cfvvd <cfvvde@gmail.com>
2023-07-19 21:52:50 +0300

    cfvvde

--- first_git.py
+++ first_git.py
@@ -1,14 +1,18 @@
 str = input()
 x = 0
 if len(str) % 2 == 1:
-    print("false")
+    stringg = list(str)
+    x+=1
+    for i in range(int(len(stringg) / 2)):
+        if stringg[i] == stringg[-i-1]:
+            x += 1
 else:
     stringg = list(str)
     for i in range(int(len(stringg)/2)):
-        print(stringg[i], stringg[-i-1])
         if stringg[i] == stringg[-i-1]:
             x += 1
-if x == (int(len(stringg))/2):
+
+if x >= (int(len(stringg))/2):
     print("True")
 else:
     print("False")
